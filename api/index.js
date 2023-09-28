11832745af0a504d97845e573848d6ea8b23882d
Rounak Kumar Arya <rounakkumar040@gmail.com>
2023-09-28 22:12:50 +0530

    create api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log(err));
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server running at port 3000");
