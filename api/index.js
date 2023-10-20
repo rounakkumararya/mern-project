8c632107327f0a1cce74fe0a931a1d78a855a2ef
Rounak Kumar Arya <rounakkumar040@gmail.com>
2023-10-20 12:04:45 +0530

    add new page and functionality

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -17,8 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRouter);
-
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.listen(3000, () => {
   console.log("Server running at port 3000");
